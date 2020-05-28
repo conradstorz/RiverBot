ab463054ff7a0a615f643c3df371912d17729dba
Conrad Storz <conradstorz@gmail.com>
2020-05-28 08:39:38 -0400

    save point

---
+++ RiverBot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""River monitor bot program.
+"""
+
+# import needed routines
+
+# import settings (twitter credentials, database details)
+
+# declare logging parameters
+
+# start loop
+
+    # check river database
+    # tweet status if needed and 
+        # update tweet database
+        # update any local display devices
