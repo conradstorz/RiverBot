3748c2ba79d148b06d94f2c5880db802f1418d3a
Conrad Storz <conradstorz@gmail.com>
2020-10-04 21:31:09 -0400

    minor code clean-up

--- data_2_csv.py
+++ data_2_csv.py
@@ -17,13 +17,15 @@ def write_csv(data, filename="temp.csv", directory="CSV_DATA"):
     pathobj = check_and_validate(filename, dirobj)
     with open(pathobj, "w", newline="") as csvfile:
         headers = data[0].keys()
-        # datalist = [item.values() for item in data]
-
         # creating a csv dict writer object
         writer = csv.DictWriter(csvfile, fieldnames=headers)
-
         # writing headers (field names)
         writer.writeheader()
-
         # writing data rows
         writer.writerows(data)
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    this_file = Path(__file__)
+    print(f'This file {this_file} has no current standalone function.')
